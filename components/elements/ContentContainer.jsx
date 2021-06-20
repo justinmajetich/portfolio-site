f57d78c93648c8512013a9dfb11347da8e1dee23
justinmajetich <justinmajetich@gmail.com>
2021-06-20 18:27:37 -0400

    Delete comment

--- components/elements/ContentContainer.jsx
+++ components/elements/ContentContainer.jsx
@@ -4,7 +4,6 @@ const StyledContentContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    /* justify-content: space-between; */
 
     width: 100%;
     height: 100%;
