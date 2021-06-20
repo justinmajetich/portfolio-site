91453b638026ba7fd3ca56be869c32d441d55f6c
justinmajetich <justinmajetich@gmail.com>
2021-06-20 18:29:23 -0400

    Create project layout

--- components/layouts/ProjectLayout.jsx
+++ components/layouts/ProjectLayout.jsx
@@ -1,3 +1,14 @@
+import PageContainer from 'components/elements/PageContainer'
+import Header from 'components/templates/Header'
+import Footer from 'components/templates/Footer'
+import ProjectContent from 'components/templates/ProjectContent'
+
 export default function ProjectLayout(props) {
-    return <p>This is a project.</p>
+    return (
+        <PageContainer>
+            <Header />
+            <ProjectContent />
+            <Footer />
+        </PageContainer>
+    )
 }
