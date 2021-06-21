f34e9a43c956e63949bb188e641dc3e69ff02e65
justinmajetich <justinmajetich@gmail.com>
2021-06-21 19:11:40 -0400

    Implement ImageGallery, use vh for more dynamic top margin

--- components/templates/ProjectContent.jsx
+++ components/templates/ProjectContent.jsx
@@ -1,21 +1,32 @@
 import styled from 'styled-components'
 import ContentContainer from "components/elements/ContentContainer";
-import ProjectBanner from "components/elements/ProjectBanner";
+import ProjectCoverImage from "components/elements/ProjectCoverImage";
 import ProjectDescription from "components/modules/ProjectDescription";
+import ImageGallery from 'components/modules/ImageGallery';
 
 const StyledContainer = styled.div`
     /* This margin conpensates for the banner.
      * It should always be equal to banner + header height.
      **/
-    margin-top: 150px;
+    margin-top: 30vh;
 `
 
 export default function ProjectContent(props) {
     return (
         <StyledContainer>
-            <ProjectBanner />
+            <ProjectCoverImage />
             <ContentContainer>
                 <ProjectDescription />
+                <h1>Gallery</h1>
+                <ImageGallery 
+                    images={[
+                        {src: '/images/vr-wheelchair.jpg', description: 'VR Wheelchair Logo'},
+                        {src: '/images/resume.png', description: 'Resume'},
+                        {src: '/images/rest-for-the-wicked.png', description: 'REST for the Wicked - Main Interface'},
+                        {src: '/images/cards/heaven-sq-300px.png', description: 'Album Cover'},
+                        {src: '/images/vr-wheelchair-demo.gif'}
+                    ]}
+                />
             </ContentContainer>
         </StyledContainer>
     )
