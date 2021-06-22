8be2b97e1d7d5a4f0525d9de374cb71a908bf875
justinmajetich <justinmajetich@gmail.com>
2021-06-22 10:12:48 -0400

    Add CatergoryNavButton components

--- components/templates/LandingContent.jsx
+++ components/templates/LandingContent.jsx
@@ -1,18 +1,42 @@
 import styled from 'styled-components'
 import ContentContainer from 'components/elements/ContentContainer'
-import SectionNavigation from 'components/templates/SectionNavigation'
-import LandingIntroduction from 'components/modules/LandingIntroduction'
+import LandingDescription from 'components/modules/LandingDescription'
 import SocialsBar from 'components/modules/SocialsBar'
+import CategoryNavButton from 'components/elements/CategoryNavButton'
 
 const StyledSocialsBarWrapper = styled.div`
     padding: 30px;
 `
 
+const StyledNavContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+
+    width: 100%;
+
+    margin-top: 10px;
+`
+
 export default function LandingContent(props) {
     return (
         <ContentContainer>
-            <LandingIntroduction />
-            <SectionNavigation />
+            <LandingDescription />
+            <StyledNavContainer>
+                <CategoryNavButton
+                    name={'Code'}
+                    sectionID={'code'}
+                />
+                <CategoryNavButton
+                    name={'Art Dept'}
+                    sectionID={'art-dept'}
+                />
+                <CategoryNavButton
+                    name={'Music'}
+                    sectionID={'music'}    
+                />
+            </StyledNavContainer>
             <StyledSocialsBarWrapper>
                 <SocialsBar />
             </StyledSocialsBarWrapper>
