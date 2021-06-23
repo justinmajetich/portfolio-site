37dec9c3e0b7f8480b2d09ddb89626bf97977b5b
justinmajetich <justinmajetich@gmail.com>
2021-06-23 08:38:27 -0400

    Implement getStaticPaths, getStaticProps

--- pages/[categoryID]/[projectID]/index.js
+++ pages/[categoryID]/[projectID]/index.js
@@ -1,7 +1,7 @@
 import ProjectLayout from 'components/layouts/ProjectLayout'
 import Head from 'next/head'
 
-export default function Section() {
+export default function Project({ content }) {
   return (
     <>
       <Head>
@@ -9,7 +9,40 @@ export default function Section() {
         <meta name="description" content="Justin Majetich's portfolio site" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ProjectLayout />
+      <ProjectLayout content={content}/>
     </>
   )
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+
+  // Call an external API endpoint to get posts
+  const res = await fetch('http://localhost:1337/projects')
+  const projects = await res.json()
+
+  // Get the paths we want to pre-render based on posts
+  const paths = projects.map((project) => ({
+    params: {
+      categoryID: project.categories[0].slug,
+      projectID: project.slug
+    },
+  }))
+
+  return { paths, fallback: false }
+}
+
+  // params contains the 'categoryID'.
+  export async function getStaticProps({ params }) {
+
+  // If the route is like '/categories/code', then params.categoryID is 'code'
+  const res = await fetch(`http://localhost:1337/projects?slug=${params.projectID}`)
+
+  const data = await res.json()
+
+  return { 
+    props: {
+      content: data[0]
+    } 
+  }
 }
