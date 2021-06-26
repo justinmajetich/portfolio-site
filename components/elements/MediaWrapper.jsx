de452150583a67c0382a86b051523492731a8279
justinmajetich <justinmajetich@gmail.com>
2021-06-26 10:36:00 -0400

    Implement wrapper to dynamically receive various media types

---
+++ components/elements/MediaWrapper.jsx
@@ -0,0 +1,55 @@
+import Image from 'next/image'
+import styled from 'styled-components'
+import ReactPlayer from 'react-player'
+
+const StyledMediaWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const StyledReactPlayerWrapper = styled.div`
+    position: relative;
+    padding-top: 56.25%;
+    width: 100%;
+
+    .react-player {
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+`
+
+export default function MediaWrapper({ media }) {
+    return (
+        <StyledMediaWrapper>
+            {media.other_media ?
+                <StyledReactPlayerWrapper>
+                    <ReactPlayer
+                        className='react-player'
+                        url={media.other_media.url}
+                        width={'100%'}
+                        height={'100%'}
+                        controls
+                        playsinline
+
+                        config={{
+                            youtube: {
+                                playerVars: { 
+                                    modestbranding: 1,
+                                }
+                            }
+                        }}
+                    />
+                </StyledReactPlayerWrapper>
+                :
+                <Image
+                    src={'http://localhost:1337' + media.strapi_media.url}
+                    alt={media.strapi_media.alternativeText}
+                    width={media.strapi_media.width}
+                    height={media.strapi_media.height}
+                />
+            }
+        </StyledMediaWrapper>
+    )
+}
