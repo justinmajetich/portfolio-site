41ec8b1103ccd3e5bf0fcfaf011f9de4dc8e80be
justinmajetich <justinmajetich@gmail.com>
2021-06-26 11:43:40 -0400

    Rename parser file/function

--- components/templates/CategoryContent.jsx
+++ components/templates/CategoryContent.jsx
@@ -1,14 +1,14 @@
 import ContentContainer from 'components/elements/ContentContainer'
 import StyledParagraph from 'components/elements/StyledParagraph'
 import ProjectsFilter from 'components/modules/ProjectsFilter'
-import ParseTextToParagraphs from 'utils/ParseTextToParagraphs'
+import parseTextToParagraphs from 'utils/parseTextToParagraphs'
 import ProjectGallery from 'components/modules/ProjectGallery'
 
 export default function CategoryContent(props) {
     return (
         <ContentContainer>
             <h1>{props.content.title}</h1>
-            {ParseTextToParagraphs(props.content.summary, StyledParagraph)}
+            {parseTextToParagraphs(props.content.summary, StyledParagraph)}
             <ProjectsFilter fitlerTags={props.content.tags} />
             <ProjectGallery
                 projects={props.content.projects}
