e98aa27c6b598ca94d64f866ded30d21ac885962
justinmajetich <justinmajetich@gmail.com>
2021-07-01 20:57:54 -0400

    Animate background gradient

--- components/elements/CategoryNavButton.jsx
+++ components/elements/CategoryNavButton.jsx
@@ -1,50 +1,99 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import { useState } from 'react'
 import styled, { keyframes } from 'styled-components'
+
+const bgPulse = props => keyframes`
+    0% { transform: translateX(0px); }
+    50% { transform: translateX(${props.categoryID % 2 == 0 ? '15px' : '-15px'}); }
+    100% { transform: translateX(0px); }
+`
 
 const StyledButtonContainer = styled.div`
     position: relative;
+    display: flex;
+    align-items: center;
 
-    width: 80%;
-    max-width: 280px;
+    width: 100%;
+    max-width: 300px;
+    height: 120px;
 
-    margin: 20px;
+    margin-bottom: 60px;
+
+    cursor: pointer;
 `
 
 const StyledButtonBackground = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+
     width: 100%;
-    height: 75px;
+    height: 100%;
 
-    border: 1px solid black;
-    border-radius: 10px;
+    border-radius: 50px;
 
-    background: lightgray;
+    background-image: linear-gradient(to ${props => props.categoryID % 2 == 0 ? 'left' : 'right'}, ${props => props.theme[props.categoryID]}, 35%, rgba(255, 255, 255, 0) 95%);
+    background-size: ${props => props.isHovered ? '200% 100%' : '80% 100%'};
+    background-position: ${props => props.categoryID % 2 == 0 ? 'right' : 'left'};
+    background-repeat: no-repeat;
+
+    filter: ${props => props.isHovered ? 'blur(35px)' : 'blur(15px)'};
+
+    transition: 1s ease-in-out;
+
+    animation: ${bgPulse} 2.5s ${props => props.categoryID / 3}s cubic-bezier(0.42, 0.0, 0.58, 1.0) infinite;
 `
 
-const StyledButton = styled.button`
-    position: absolute;
-    top: -8px;
-    left: -8px;
+const StyledButtonContent = styled.div`
+    display: flex;
+    flex-direction: column;
 
     width: 100%;
-    height: 75px;
+    padding: 0 30px;
 
-    border: 1px solid black;
-    border-radius: 10px;
+    text-align: ${props => props.categoryID % 2 == 0 ? 'right' : 'left'};
 
-    background: white;
-    cursor: pointer;
 
-    transition: top .05s, left .05s;
+    h2 {
+        margin: 0;
+    }
+
+    p {
+        font-size: 1em;
+        margin: 0;
+    }
 `
 
 export default function Button(props) {
+
+    const [isHovered, setIsHovered] = useState(false)
+
+    const handleMouseEnter = () => {
+        setIsHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+    }
+
     return (
         <Link href={props.sectionID}>
-            <StyledButtonContainer>
-                <StyledButtonBackground/>
-                <StyledButton>
-                    {props.name ? props.name : 'Button'}
-                </StyledButton>
+            <StyledButtonContainer
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <StyledButtonBackground
+                    categoryID={props.categoryID}
+                    isHovered={isHovered}
+                />
+                <StyledButtonContent
+                    categoryID={props.categoryID}
+                >
+                    <h2>{props.title}</h2>
+                    <p>{props.description}</p>
+                </StyledButtonContent>
             </StyledButtonContainer>
         </Link>
     )
