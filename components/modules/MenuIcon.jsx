0e99263342430e02f6eb6d8d76abca2e2e9ccb23
justinmajetich <justinmajetich@gmail.com>
2021-07-02 13:39:59 -0400

    Implement lottie player

--- components/modules/MenuIcon.jsx
+++ components/modules/MenuIcon.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import Lottie from 'react-lottie'
+import menuAnimation from '/public/lotties/hamburger-collapse-2.json'
 import Icon from '../elements/Icon'
 
 const StyledWrapper = styled.div`
@@ -14,14 +16,31 @@ const StyledWrapper = styled.div`
     cursor: pointer;
 `
 
+const defaultLottieOptions = {
+    loop: true,
+    autoplay: false,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
 export default function MenuIcon(props) {
     return (
         <StyledWrapper
             onClick={() => props.setMenuIsActive(!props.menuIsActive)}
         >
-            <Icon
+            {/* <Icon
                 type={props.menuIsActive ? 'x' : 'hamburger'}
                 alt={'Navigation Menu'}
+            /> */}
+            <Lottie
+                options={{
+                    ...defaultLottieOptions,
+                    animationData: menuAnimation
+                }}
+                height={200}
+                width={200}
+                isStopped={!props.menuIsActive}
             />
         </StyledWrapper>
     )
--- config/apiURL.js
+++ config/apiURL.js
@@ -1,1 +1,1 @@
-export const apiURL = process.env.NODE_ENV === 'production' ? process.env.API_URL : process.env.DEV_API_URL
+export const apiURL = process.env.NODE_ENV === 'production' ? process.env.API_URL : process.env.API_URL
