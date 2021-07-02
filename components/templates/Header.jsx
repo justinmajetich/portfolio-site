79c693ab95975a987e04b5402ce14b02f5016cce
justinmajetich <justinmajetich@gmail.com>
2021-07-02 11:36:48 -0400

    Add initials to menu active state

--- components/templates/Header.jsx
+++ components/templates/Header.jsx
@@ -1,5 +1,7 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import Navigation from 'components/modules/Navigation'
+import { fadeIn } from 'utils/animations'
+
 
 const StyledContainer = styled.div`
     position: sticky;
@@ -9,8 +11,7 @@ const StyledContainer = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: flex-end;
-
+    justify-content: ${props => props.menuIsActive ? 'space-between' : 'flex-end'};
 
     width: 100%;
     height: 50px;
@@ -18,10 +19,23 @@ const StyledContainer = styled.div`
     padding: 15px;
 `
 
+const StyledInitials = styled.h3`
+    position: relative;
+    z-index: 2;
+
+    animation: ${fadeIn} .25s 1;
+`
+
 export default function Header(props) {
     return (
-        <StyledContainer>
+        <StyledContainer menuIsActive={props.menuIsActive}>
+            {props.menuIsActive ?
+                <StyledInitials>
+                    {'JM'}
+                </StyledInitials>
+            : null}
             <Navigation
+                currentPageID={props.pageID}
                 categoryID={props.categoryID}
                 menuIsActive={props.menuIsActive}
                 setMenuIsActive={props.setMenuIsActive}
