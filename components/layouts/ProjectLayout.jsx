861eaa26ff40113d4e9cc7f82e45a1c6cbfc5d4a
justinmajetich <justinmajetich@gmail.com>
2021-07-01 21:13:54 -0400

    Implement dynamic menu theming

--- components/layouts/ProjectLayout.jsx
+++ components/layouts/ProjectLayout.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react'
 import PageContainer from 'components/elements/PageContainer'
 import Header from 'components/templates/Header'
 import Footer from 'components/templates/Footer'
 import ProjectContent from 'components/templates/ProjectContent'
 
 export default function ProjectLayout({ content }) {
+
+    const [menuIsActive, setMenuIsActive] = useState(false)
+
     return (
         <PageContainer>
-            <Header />
-            <ProjectContent
-                content={content}
+            <Header
+                categoryID={content.categories[0].id}
+                menuIsActive={menuIsActive}
+                setMenuIsActive={setMenuIsActive}
             />
+            {!menuIsActive ?
+                <ProjectContent content={content}/>
+            : null}
             <Footer />
         </PageContainer>
     )
