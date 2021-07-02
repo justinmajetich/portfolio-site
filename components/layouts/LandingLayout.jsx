01b6e7db1cf3d38a35c52c272dc598cbcf7405ac
justinmajetich <justinmajetich@gmail.com>
2021-07-01 21:14:29 -0400

    Implement dynamic menu theming

--- components/layouts/LandingLayout.jsx
+++ components/layouts/LandingLayout.jsx
@@ -1,13 +1,24 @@
 
+import { useState } from 'react'
 import PageContainer from 'components/elements/PageContainer'
 import Header from 'components/templates/Header'
+import Footer from 'components/templates/Footer'
 import LandingContent from 'components/templates/LandingContent'
 
-export default function LandingLayout(props) {
+export default function LandingLayout() {
+
+    const [menuIsActive, setMenuIsActive] = useState(false)
+
     return (
         <PageContainer>
-            <Header isLanding={true}/>
-            <LandingContent />
+            <Header
+                menuIsActive={menuIsActive}
+                setMenuIsActive={setMenuIsActive}
+            />
+            {!menuIsActive ?
+                <LandingContent />
+            : null}
+            <Footer />
         </PageContainer>
     )
 }
