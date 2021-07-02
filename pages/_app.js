a25077ab147311ed4a68a06150a61ae1c6c99036
justinmajetich <justinmajetich@gmail.com>
2021-07-01 20:30:04 -0400

    Add global theming

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const theme = {
-
+  1: '#66E1A6',
+  2: '#DB9BF1',
+  3: '#FFE486',
+  darkGray: '#606060',
+  lightGray: '#F0F0F0'
 }
 
 const GlobalStyle = createGlobalStyle`
@@ -11,10 +15,22 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    -webkit-tap-highlight-color: transparent;
   }
 
   h1 {
+    margin: 0px 0px 20px 0px;
+    font-size: 3.5em;
+  }
+
+  h2 {
     margin: 20px 0;
+    font-size: 2em;
+  }
+
+  h3 {
+    margin: 10px 0;
+    font-size: 1.5em;
   }
 
   p {
