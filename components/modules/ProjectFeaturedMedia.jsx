d0a57b6991c171f69e009dfb72f96f2066daa5c3
justinmajetich <justinmajetich@gmail.com>
2021-07-07 21:17:10 -0400

    Add wrapper to center content

--- components/modules/ProjectFeaturedMedia.jsx
+++ components/modules/ProjectFeaturedMedia.jsx
@@ -2,16 +2,23 @@ import MediaWrapper from "components/elements/MediaWrapper";
 import styled from 'styled-components'
 
 const StyledWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`
+
+const StyledContainer = styled.div`
     width: 100%;
     max-width: 640px;
-
     margin: 40px 0;
 `
 
 export default function ProjectFeaturedMedia({ media }) {
     return (
         <StyledWrapper>
-            <MediaWrapper media={media}/>
+            <StyledContainer>
+                <MediaWrapper media={media}/>
+            </StyledContainer>
         </StyledWrapper>
     )
 }
