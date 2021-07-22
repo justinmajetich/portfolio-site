ae1597a8f03c0d6a56b2a0fd5c900896e31ea830
justinmajetich <justinmajetich@gmail.com>
2021-07-22 14:31:25 -0400

    Format URL for request to Google Font API

---
+++ utils/getGoogleFontsURL.js
@@ -0,0 +1,11 @@
+/**
+ * Formats font name to pass a parameter then concatenates to Google Fonts API stub.
+ * @param {string} fontName Name of the Google Font.
+ * @returns Formatted URL
+ */
+export default function GetGoogleFontsURL(fontName) {
+    
+    const url = 'https://fonts.googleapis.com/css2?family=' + fontName.replace(/\s/g, '+')
+
+    return url
+}
