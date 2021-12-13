932d76199551c62d9dcc0230af57041d5a7d4be2
justinmajetich <justinmajetich@gmail.com>
2021-12-13 15:26:09 -0500

    Add responsive jsx elements

--- utils/media-breakpoints.js
+++ utils/media-breakpoints.js
@@ -1,3 +1,5 @@
+import { useMediaQuery } from 'react-responsive'
+
 const size = {
     mobileS: '320px',
     mobileM: '375px',
@@ -18,3 +20,20 @@ const size = {
     desktop: `(min-width: ${size.desktop})`,
     desktopL: `(min-width: ${size.desktop})`
   };
+
+  export const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: size.laptop})
+    return isDesktop ? children : null
+  }
+  export const Tablet = ({ children }) => {
+    const isTablet = useMediaQuery({ minWidth: size.tablet, maxWidth: (parseInt(size.laptop) - 1) })
+    return isTablet ? children : null
+  }
+  export const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({maxWidth: (parseInt(size.laptop) - 1 )})
+    return isMobile ? children : null
+  }
+  export const Default = ({ children }) => {
+    const isNotMobile = useMediaQuery({ minWidth: size.laptop })
+    return isNotMobile ? children : null
+  }
