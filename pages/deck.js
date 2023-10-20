0c211d02e68810c4bb2fb17710e68c647574b4af
Justin <justinmajetich@gmail.com>
2023-10-20 12:12:11 -0400

    Make responsice for mobile

--- pages/deck.js
+++ pages/deck.js
@@ -4,41 +4,88 @@ import { withTheme } from 'styled-components'
 import Header from 'components/templates/Header'
 import Footer from 'components/templates/Footer'
 import GetGoogleFontsURL from 'utils/getGoogleFontsURL'
-import PageContainer from 'components/elements/PageContainer'
 import ContentContainer from 'components/elements/ContentContainer'
 import Image from 'next/image'
+import { fadeIn } from 'utils/animations'
 import ReactPlayer from 'react-player'
+import { device } from 'utils/media-breakpoints'
 
-const StyledPageBackground = styled.div`
-    position: fixed;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
-`
-const Spacer = styled.div`
-    margin: 50px;
+const StyledSpacer = styled.div`
+    margin: 25px;
+
+    @media ${device.laptop} {
+        margin: 50px;
+    }
 `
 const MediaRowWrapper = styled.div`
     display: flex;
+    flex-direction: column;
     width: 100%;
-    justify-content: center;
+    align-content: center;
+
+    @media ${device.laptop} {
+        flex-direction: row;
+        justify-content: center;
+    }
 `
 const MediaRowImage = styled.div`
+    display: flex;
+    justify-content: center;
     margin: 20px;
 `
 const MediaCaption = styled.div`
-    display: flex;
     width: 100%;
+    max-width: 320px;
+    margin: 0px 10px;
+    display: flex;
     color: gray;
     padding: 10px 0;
     justify-content: center;
+    text-align: center;
+
+    @media ${device.laptop} {
+        max-width: 720px;
+    }
 `
 const VideoWrapper = styled.div`
-    width: 730px;
+    width: 100%;
+
+    @media ${device.laptop} {
+        width: 720px;
+    }
 `
 const VideoWrapperSmall = styled.div`
     width: 360px;
 `
+const StyledPageContainer = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100%;
+    min-height: 100vh;
+
+    padding-bottom: 350px;
+
+    animation: ${fadeIn} 0.25s;
+`
+const StyledContentContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
+const Divider = styled.div`
+    width: 80px;
+    border-top: 1px solid lightgray;
+`
+
+function Spacer({ noDividerLine }) {
+    return (
+        <StyledSpacer>
+            {!noDividerLine && <Divider />}
+        </StyledSpacer>
+    )
+}
+
 function Deck({ content, theme}) {
   return (
     <>
@@ -48,57 +95,69 @@ function Deck({ content, theme}) {
             <link rel="icon" href="/favicon.ico" />
             <link rel="stylesheet" href={GetGoogleFontsURL(theme.headerFont)}></link>
         </Head>
-        <PageContainer>
-            <StyledPageBackground />
+        <StyledPageContainer>
             <>
-            <Header />
+                <Header />
+                <StyledContentContainer>
                 <ContentContainer>
-                    <Spacer />
-                    <h2>{'Justin Majetich - Design & Animation'}</h2>
+                    <Spacer noDividerLine/>
+                    <h2>{'Justin Majetich'}</h2>
+                    <h3 style={{color: 'gray'}}>{'Design & Animation'}</h3>
                     <Spacer />
 
-                    <Image
-                    src={'https://res.cloudinary.com/justinmajetich/image/upload/v1676063540/Races_Cover_3_K_ad4571e823.jpg'}
-                    alt={'Races Cover'}
-                    width={720}
-                    height={720}
-                    />
+                    <MediaRowImage>
+                        <Image
+                        src={'https://res.cloudinary.com/justinmajetich/image/upload/v1676063540/Races_Cover_3_K_ad4571e823.jpg'}
+                        alt={'Races Cover'}
+                        width={720}
+                        height={720}
+                        />
+                    </MediaRowImage>
                     <MediaCaption>{'\"Races\" - cover art for Barrie\'s 5K EP'}</MediaCaption>
                     <Spacer />
 
-                    <Image
-                    src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Ghost_World_Cover_3_K_4963d123c2.jpg'}
-                    alt={'Ghost World Cover'}
-                    width={720}
-                    height={720}
-                    />
+                    <MediaRowImage>
+                        <Image
+                        src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Ghost_World_Cover_3_K_4963d123c2.jpg'}
+                        alt={'Ghost World Cover'}
+                        width={720}
+                        height={720}
+                        />
+                    </MediaRowImage>
                     <MediaCaption>{'\"Ghost World\" - single cover art for Barrie'}</MediaCaption>
                     <Spacer />
 
-                    <Image
-                    src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Empty_Cover_3_K_babc005073.jpg'}
-                    alt={'Empty'}
-                    width={720}
-                    height={720}
-                    />
+                    <MediaRowImage>
+                        <Image
+                        src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Empty_Cover_3_K_babc005073.jpg'}
+                        alt={'Empty'}
+                        width={720}
+                        height={720}
+                        />
+                    </MediaRowImage>
                     <MediaCaption>{'\"Empty\" - single cover art for Barrie'}</MediaCaption>
                     <Spacer />
 
-                    <Image
-                    src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Unholy_Appetite_Single_Art_3_K_fde4ab0344.jpg'}
-                    alt={'Unholy Appetite Cover'}
-                    width={720}
-                    height={720}
-                    />
+                    
+                    <MediaRowImage>
+                        <Image
+                        src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Unholy_Appetite_Single_Art_3_K_fde4ab0344.jpg'}
+                        alt={'Unholy Appetite Cover'}
+                        width={720}
+                        height={720}
+                        />
+                    </MediaRowImage>
                     <MediaCaption>{'\"Unholy Appetite\" - single cover art for Barrie'}</MediaCaption>
                     <Spacer />
 
-                    <Image
-                    src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Nocturne_Interlude_Cover_3_K_568caa9745.jpg'}
-                    alt={'Nocturne Cover'}
-                    width={720}
-                    height={720}
-                    />
+                    <MediaRowImage>
+                        <Image
+                        src={'https://res.cloudinary.com/justinmajetich/image/upload/v1697664740/Nocturne_Interlude_Cover_3_K_568caa9745.jpg'}
+                        alt={'Nocturne Cover'}
+                        width={720}
+                        height={720}
+                        />
+                    </MediaRowImage>
                     <MediaCaption>{'\"Nocturne\" - single cover art for Barrie'}</MediaCaption>
                     <Spacer />
 
@@ -209,35 +268,39 @@ function Deck({ content, theme}) {
                     <MediaCaption>{'Animations for Barrie\'s 5K'}</MediaCaption>
                     <Spacer />
 
-                    <VideoWrapper>
-                        <ReactPlayer
-                            className='react-player'
-                            url={'https://res.cloudinary.com/justinmajetich/video/upload/v1697666521/Barrie_SXSW_Anim_3840x3840_1cad7ac140.mp4'}
-                            width={'100%'}
-                            height={'100%'}
-                            controls
-                            playsinline
+                    <MediaRowImage>
+                        <VideoWrapper>
+                            <ReactPlayer
+                                className='react-player'
+                                url={'https://res.cloudinary.com/justinmajetich/video/upload/v1697666521/Barrie_SXSW_Anim_3840x3840_1cad7ac140.mp4'}
+                                width={'100%'}
+                                height={'100%'}
+                                controls
+                                playsinline
 
-                            config={{
-                                youtube: {
-                                    playerVars: { 
-                                        modestbranding: 1,
+                                config={{
+                                    youtube: {
+                                        playerVars: { 
+                                            modestbranding: 1,
+                                        }
                                     }
-                                }
-                            }}
-                        />
-                    </VideoWrapper>
+                                }}
+                            />
+                        </VideoWrapper>
+                    </MediaRowImage>
                     <MediaCaption>{'3D character design and animation for Barrie\'s SXSW promotional media'}</MediaCaption>
                     <Spacer />
 
-                    <Image
-                    src={'https://res.cloudinary.com/justinmajetich/image/upload/v1671216646/child_of_saints_dark_square_9a38294ab2.jpg'}
-                    alt={'Child of Saints - Main'}
-                    width={720}
-                    height={720}
-                    />
+                    <MediaRowImage>
+                        <Image
+                        src={'https://res.cloudinary.com/justinmajetich/image/upload/v1671216646/child_of_saints_dark_square_9a38294ab2.jpg'}
+                        alt={'Child of Saints - Main'}
+                        width={720}
+                        height={720}
+                        />
+                    </MediaRowImage>
                     <MediaCaption>{'\"Child of Saints\" - 3D character design and sculpt'}</MediaCaption>
-                    <Spacer />
+                    <Spacer noDividerLine/>
 
                     <MediaRowWrapper>
                         <MediaRowImage>
@@ -268,12 +331,14 @@ function Deck({ content, theme}) {
                     <MediaCaption>{'\"Child of Saints\" - clay & details'}</MediaCaption>
                     <Spacer />
 
-                    <Image
-                    src={'https://res.cloudinary.com/justinmajetich/image/upload/v1625695153/LITR_BOOK_1000px_8ff8146bd8.gif'}
-                    alt={'LUV IN THE RUINS - Book'}
-                    width={1000}
-                    height={773}
-                    />
+                    <MediaRowImage>
+                        <Image
+                        src={'https://res.cloudinary.com/justinmajetich/image/upload/v1625695153/LITR_BOOK_1000px_8ff8146bd8.gif'}
+                        alt={'LUV IN THE RUINS - Book'}
+                        width={1000}
+                        height={773}
+                        />
+                    </MediaRowImage>
                     <MediaCaption>{'LUV IN THE RUINS art book - text, layout & design'}</MediaCaption>
                     <Spacer />
 
@@ -304,13 +369,14 @@ function Deck({ content, theme}) {
                         </MediaRowImage>
                     </MediaRowWrapper>
                     <MediaCaption>{'Layout & design for Barrie\'s physical 5K releases'}</MediaCaption>
-                    <Spacer />
+                    <Spacer noDividerLine/>
 
                 </ContentContainer>
-                <Spacer />
+                <Spacer noDividerLine />
+                </StyledContentContainer>
                 <Footer />
             </>
-        </PageContainer>
+        </StyledPageContainer>
     </>
   )
 }
